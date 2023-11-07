21911be2a4a9e093984fbe8f45d2b1eb2d4c0aa9
Sepang2 <sepang_2@naver.com>
2023-11-07 18:06:31 +0900

    Change date color

--- src/components/DateCard.jsx
+++ src/components/DateCard.jsx
@@ -2,7 +2,7 @@ const DateCard = ({ date, todo }) => {
   return (
     <li className="bg-gray-50 w-32 h-32 rounded-lg shadow-xl flex flex-col">
       <div
-        className={`bg-blue-300 ${date % 7 === 4 && "bg-orange-300"} ${
+        className={`bg-gray-300 ${date % 7 === 4 && "bg-blue-300"} ${
           date % 7 === 5 && "bg-red-300"
         }
         } p-2 rounded-t-lg`}
